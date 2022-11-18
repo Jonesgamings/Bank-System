df5a12ab5c1907fa3b4480699df6735e86f3b340
Jonesgamings <harry.tennyson.jones@gmail.com>
2022-11-18 19:42:33 +0000

    Added Card Class

--- Bank System/Account.h
+++ Bank System/Account.h
@@ -1,6 +1,18 @@
 #pragma once
+#include "Card.h"
+
 class Account
 {
-	
-};
+	int accountNumber;
+	Card card;
 
+	Account()
+	{
+		this->createDetails();
+	}
+
+	void createDetails()
+	{
+
+	}
+};
---
+++ Bank System/Card.h
@@ -0,0 +1,17 @@
+#pragma once
+class Card
+{
+	int cardNumber;
+	int pin;
+
+	int resetPin()
+	{
+		return 0;
+	}
+
+	int resetCardNumber()
+	{
+		return 0;
+	}
+};
+
